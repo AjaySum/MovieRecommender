91a4ad57604190292c5ba334f72d8db840e75fbd
ajaysum <ajaysum@umich.edu>
2024-03-22 18:12:39 -0400

    preprocessing

--- movrec/main.py
+++ movrec/main.py
@@ -1,14 +1,14 @@
-import utils
-from datasets import load_dataset
-movies = load_dataset("vishnupriyavr/wiki-movie-plots-with-summaries")
 import sys
 import string
-
+import nltk
+from nltk.corpus import stopwords
+from datasets import load_dataset
+nltk.download('stopwords')
 
 def clean_movie_text(text):
     punctuation_string = string.punctuation + "…"
+    stop_words = set(stopwords.words('english'))
 
-   
     cleaned_content = []
     for line in text:
         clean_line = ''
@@ -17,17 +17,16 @@ def clean_movie_text(text):
                 clean_line += "'"
             elif char not in punctuation_string:
                 clean_line += char
-        cleaned_content.append(clean_line.lower())
+        cleaned_tokens = [word.lower() for word in clean_line.split() if word.lower() not in stop_words]
+        cleaned_content.append(' '.join(cleaned_tokens))
 
     return cleaned_content
 
 if __name__ == "__main__":
-    
     if len(sys.argv) != 2:
         print("Usage: python main.py <file_path>")
         sys.exit(1)
 
-    
     file_path = sys.argv[1]
     movies_dataset = load_dataset(file_path)
     cleaned_movies = []
