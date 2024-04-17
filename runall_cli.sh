ced0edaa6f52320a0c2c856412953e507d66c69b
Richard Wang <riwa@umich.edu>
2024-04-17 17:48:57 -0400

    fixing things

--- runall_cli.sh
+++ runall_cli.sh
@@ -6,7 +6,5 @@ cd movrec
 python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
-python3 preprocess.py movies_dataset.csv
-python3 similarities.py
 python3 calculate.py
 cd ..
