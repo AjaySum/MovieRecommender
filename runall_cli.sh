ae2bc7151d8e1e140a14b03fe2d8aa8d2b392f34
Richard Wang <riwa@umich.edu>
2024-04-17 17:37:50 -0400

    cli script

--- runall_cli.sh
+++ runall_cli.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
+set -Eeuo pipefail
+set -x  # print commands
+
 cd movrec
-./bin/install
+python3 -m venv env
+source env/bin/activate
+pip install -r requirements.txt
 python3 preprocess.py movies_dataset.csv
 python3 similarities.py
 python3 calculate.py
