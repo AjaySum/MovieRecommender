00a515a0528ea4726995facde1e949033dd05017
shrujain <shrujain@umich.edu>
2024-04-18 11:01:42 -0400

    commented

--- movrec/similarities.py
+++ movrec/similarities.py
@@ -16,36 +16,42 @@ nltk.download('punkt')
 class SimilarityCalculator():
     fullPlotsDoc = ""
     summariesDoc = ""
+
+    # load in pretrained GloVe for word embeddings
     dir = os.path.join(os.path.dirname(os.getcwd()), "glove.6B")
     wordVectors = Vectors(name=os.path.join(dir, 'glove.6B.300d.txt'))
 
-    # key is tuple (id1, id2) and value is similarity score
-    summariesSimilarity = {}
-    fullPlotSimilarity = {}
-    fullPlotVecs = {} # sorted by id
-    summaryVecs = {} # sorted by id
-    fullPlots = []
+    fullPlotVecs = {} # id --> full plot embedding vec
+    summaryVecs = {} # id --> summary embedding vec
 
-    ids = []
 
     def __init__(self, fullPlotsDoc, summariesDoc):
+        # find doc in directory created by preprocess
         self.fullPlotsDoc = f"preprocess_output/{fullPlotsDoc}"
         self.summariesDoc = f"preprocess_output/{summariesDoc}"
     
     def makeEmbeddings(self):
         with open(self.fullPlotsDoc, 'r') as file:
+            # skip the line with schema
             next(file)
             for line in file:
                 line = line.strip()
+                # clean 
+                line = line.replace('"', '')
+                # separate id and text
                 items = line.split('|')
-                self.ids.append(int(items[0]))
+                # store embedding vector for id
                 self.fullPlotVecs[int(items[0])] = self.createEmbeddingVec(items[1])
         with open(self.summariesDoc, 'r') as file:
+            # skip the line with schema
             next(file)
             for line in file:
                 line = line.strip()
+                # clean
                 line = line.replace('"', '')
+                # separate id and text
                 items = line.split('|')
+                # store embedding vector for id
                 self.summaryVecs[int(items[0])] = self.createEmbeddingVec(items[1])
         
         current_directory = os.getcwd()
@@ -53,20 +59,24 @@ class SimilarityCalculator():
         if not os.path.exists(final_directory):
             os.makedirs(final_directory)
 
+        # create pickle files
         with open('similarities_output/fullPlotEmbeddings.pkl', 'wb') as f:
-            # Pickle the dictionary and write it to the file
+            # Pickle the full plot dictionary and write it to the file
             pickle.dump(self.fullPlotVecs, f)
         
         with open('similarities_output/summaryEmbeddings.pkl', 'wb') as f:
-            # Pickle the dictionary and write it to the file
+            # Pickle the summary dictionary and write it to the file
             pickle.dump(self.summaryVecs, f)
         
         
 
 
     def createEmbeddingVec(self, text):
+        # create tokens
         words = nltk.word_tokenize(text.lower())
+        # get word embeddings
         word_embeddings = [self.wordVectors[word].numpy() for word in words if word in self.wordVectors.stoi]
+        # return paragraph vector or None if no word embeddings
         if word_embeddings:
             word_embeddings_array = np.array(word_embeddings)
             paragraph_vector = torch.mean(torch.tensor(word_embeddings_array), dim=0)
@@ -75,33 +85,10 @@ class SimilarityCalculator():
             return None
 
 
-    def tfIdf(self):
-        out_file = f"fullPlotTfIdfScores.txt"
-        sys.stdout = open(out_file, 'w')
-        # for item in self.summaries:
-        #     print(item)
-        tfidf_vectorizer = TfidfVectorizer()
-
-        # Fit and transform the documents
-        tfidf_matrix = tfidf_vectorizer.fit_transform(self.fullPlots)
-
-        # Compute cosine similarity
-        cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
-
-        # Print the cosine similarity matrix
-        for index, value in np.ndenumerate(cosine_sim):
-            if index[0] != index[1]:
-                index = str(index)
-                index = index.replace("(", "")
-                index = index.replace(")", "")
-                index = index.replace(",", "")
-                value = max(float(value), 0)
-                
-                print(f"{index}|{value}")
-
-
 
 def main():
+    # make an instance of the similarity calculator and pass in the documents with full plot and summary
+    # docs should have format id | text
     s = SimilarityCalculator(fullPlotsDoc="fullplot.csv", summariesDoc="summaries.csv")
     s.makeEmbeddings()
 
